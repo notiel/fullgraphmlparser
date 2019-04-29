cad9f39987a4171a57d51ae471da50b5451c7467
Julia Salnikova (Notiel) <agereth@gmail.com>
2019-04-30 02:14:45 +0300

    pep8 fixes and some of typehints

--- graphml.py
+++ graphml.py
@@ -17,26 +17,26 @@ it contains functions for analyze and transform data in .graphml file
 -get_state_label(data: dict) -> str                                    gets state label from state node
 -get_group_label(data: dict)-> str:                                    gets group node label from node data
 -is_edge_correct(edge: dict, edge_type: str) -> bool                   checks if edge is correct
--def get_edge_coordinates(edge: dict) -> tuple                         function get edge coordinates (start as a shift from
-                                                                       source state center, enf as a shift from source state
-                                                                       center)
+-def get_edge_coordinates(edge: dict) -> tuple                         function get edge coordinates (start as a shift
+                                                                       from source state center, enf as a shift from
+                                                                       source state center)
 -def get_edge_label_coordinates(edge: dict) -> tuple:                  gets coordinates for edge label
 """
-
+from typing import List, Dict, Any, Union, Tuple
 edgetype = 'y:PolyLineEdge'
 
 
-def flatten(mixed_data: list, key: str) -> list:
+def flatten(mixed_data: list, key: str) -> List:
     """
     function separates data nodes using key
     Example:
-        >>>flatten([{'data':[info1, info2]}], 'data')
+        flatten([{data:[info1, info2]}], data)
         [info1, info2]
     :param mixed_data: list of nodes with data
     :param key: dictionary key to get list with data
     :return: list with data items
     """
-    flat_data = []
+    flat_data = list()
     for node in mixed_data:
         if isinstance(node, dict) and key in node.keys():
             node_data = node[key]
@@ -48,18 +48,18 @@ def flatten(mixed_data: list, key: str) -> list:
     return flat_data
 
 
-def flatten_with_key(mixed_data: list, key: str, newkeys: dict) -> list:
+def flatten_with_key(mixed_data: List[Any], key: str, newkeys: Dict[str, Any]) -> List[Any]:
     """
     function separates data nodes using key adding new dict keys from newkeys
     Example:
-        >>>flatten_with_key([{'data' : [info1, info2]}, 'id':1, 'test':'value'], 'data', {'id':'number', 'test':'data'})
+        flatten_with_key([{'data' : [info1, info2]}, 'id':1, 'test':'value'], 'data', {'id':'number', 'test':'data'})
         [{info1, 'number':1, 'data':'value'}, {info2, 'number':1, 'data':'value'}]
     :param mixed_data: list of nodes with data
     :param key: dictionary key to get list with data
     :param newkeys: key for additional data to extract
     :return: list with data items
     """
-    flat_data = []
+    flat_data: List[Any] = list()
     for node in mixed_data:
         if isinstance(node, dict) and key in node.keys():
             node_data = node[key]
@@ -75,30 +75,31 @@ def flatten_with_key(mixed_data: list, key: str, newkeys: dict) -> list:
     return flat_data
 
 
-def get_sub_nodes(nodes: list) -> list:
+def get_sub_nodes(nodes: List[Any]) -> List[Any]:
     """
-    gets all sub-nodes from groupnodes (detects in node has subnodes (has "graph' key) and gets all nodes in node['graph']
+    gets all sub-nodes from groupnodes (
+    detects in node has subnodes (has "graph' key) and gets all nodes in node['graph']
     item, flattened by node and by data fields
     :param nodes: list of nodes
     :return: list of nodes
     """
-    group_nodes = [node for node in nodes if '@yfiles.foldertype' in node.keys()]
-    res = []
+    group_nodes: List[Any] = [node for node in nodes if '@yfiles.foldertype' in node.keys()]
+    res: List[Any] = list()
     while group_nodes:
-        new = [node['graph'] for node in group_nodes]
+        new: List[Any] = [node['graph'] for node in group_nodes]
         new = flatten(new, 'node')
         res.extend(new)
         group_nodes = [node for node in new if '@yfiles.foldertype' in node.keys()]
     return res
 
 
-def get_flat_nodes(data: dict) -> [dict]:
+def get_flat_nodes(data: Dict[str, Any]) -> List[Dict[str, Any]]:
     """
     gets list of flattened nodes from data dictg
     :param data: dict with data
     :return: list of nodes
     """
-    nodes = data['graphml']['graph']['node']
+    nodes: Union[List[Dict[str, Any], Dict[str, Any]]] = data['graphml']['graph']['node']
     if isinstance(nodes, dict):
         nodes = [nodes]
     nodes.extend(get_sub_nodes(nodes))
@@ -106,12 +107,13 @@ def get_flat_nodes(data: dict) -> [dict]:
     return nodes
 
 
-def update_qroup_nodes(data: [dict]):
+def update_qroup_nodes(data: List[Dict[str, Any]]):
     """
-
-    :param data:
+    flattens group nodes
+    :param data: data with nodes
     :return:
     """
+    correct = dict()
     for node in data:
         if is_node_a_group(node):
             group_nodes = node['y:ProxyAutoBoundsNode']['y:Realizers']['y:GroupNode']
@@ -121,18 +123,19 @@ def update_qroup_nodes(data: [dict]):
                         correct = group_node
                 node['y:ProxyAutoBoundsNode']['y:Realizers']['y:GroupNode'] = correct
 
-def get_flat_edges(data: dict) -> [dict]:
+
+def get_flat_edges(data: Dict[str, Any]) -> List[Dict[str, Any]]:
     """
     gets list of transitions from data dict
     :param data: dict with data
     :return: list of transitions
     """
-    edges = data['graphml']['graph']['edge']
-    flattened_edges = flatten_with_key(edges, 'data', {'@source': 'source', '@target': 'target'})
+    edges: List[Dict[str, Any]] = data['graphml']['graph']['edge']
+    flattened_edges: List[Dict[str, Any]] = flatten_with_key(edges, 'data', {'@source': 'source', '@target': 'target'})
     return flattened_edges
 
 
-def get_start_node_data(nodes: [dict], edges) -> tuple:
+def get_start_node_data(nodes: List[Dict[str, Any]], edges) -> Tuple[int, int, str]:
     """functions gets start node id (target of start edge) and raises exception if no start node found
     :param nodes: list of nodes
     :param edges: list of edges
@@ -146,14 +149,14 @@ def get_start_node_data(nodes: [dict], edges) -> tuple:
     for edge in edges:
         if edge['source'] == node_id and len(edge.keys()) > 3:
             if is_edge_correct(edge, edgetype) and "#text" in edge[edgetype]['y:EdgeLabel'].keys():
-                action = edge[edgetype]['y:EdgeLabel']["#text"]
+                action: str = edge[edgetype]['y:EdgeLabel']["#text"]
             else:
                 action = ""
             return node_id, edge['target'], action
-    raise ValueError                   #raise exception of no start node found
+    raise ValueError                   # raise exception of no start node found
 
 
-def is_node_a_choice(node: dict) -> bool:
+def is_node_a_choice(node: Dict[str, Any]) -> bool:
     """
     detects if node is a choice (using @configuration key)
     :param node: dict with node
@@ -165,8 +168,7 @@ def is_node_a_choice(node: dict) -> bool:
     return False
 
 
-
-def is_node_a_state(node: dict) -> bool:
+def is_node_a_state(node: Dict[str, Any]) -> bool:
     """
     detects if node is a state (using @configuration key)
     :param node: dict with node
@@ -178,7 +180,7 @@ def is_node_a_state(node: dict) -> bool:
     return False
 
 
-def is_node_a_note(node: dict) -> bool:
+def is_node_a_note(node: Dict[str, Any]) -> bool:
     """
     detects is node contains start variables
     :param node: dict with node
@@ -190,7 +192,7 @@ def is_node_a_note(node: dict) -> bool:
     return False
 
 
-def is_node_a_group(node: dict) -> bool:
+def is_node_a_group(node: Dict[str, Any]) -> bool:
     """
     detects if node if group node
     :param node: dict with node
@@ -201,7 +203,7 @@ def is_node_a_group(node: dict) -> bool:
     return False
 
 
-def get_note_label(node: dict) -> str:
+def get_note_label(node: Dict[str, Any]) -> str:
     """
     gets note_label data
     :param node:
@@ -210,7 +212,7 @@ def get_note_label(node: dict) -> str:
     return node['y:UMLNoteNode']['y:NodeLabel']["#text"]
 
 
-def get_coordinates(node: dict) -> tuple:
+def get_coordinates(node: Dict[str, Any]) -> Tuple[int, int, int, int]:
     """
     function to get node coordinates
     :param node: node with data
@@ -222,15 +224,15 @@ def get_coordinates(node: dict) -> tuple:
         'y:GroupNode']
     if 'y:Geometry' not in node.keys():
         return 0, 0, 0, 0
-    node = node['y:Geometry']
-    x = int(float(node['@x']))
-    y = int(float(node['@y']))
-    width = int(float(node['@width']))
-    height = int(float(node['@height']))
+    node: Dict[str, Any] = node['y:Geometry']
+    x: int = int(float(node['@x']))
+    y: int = int(float(node['@y']))
+    width: int = int(float(node['@width']))
+    height: int = int(float(node['@height']))
     return x, y, width, height
 
 
-def coord_sort(node: dict) -> float:
+def coord_sort(node: Dict[str, Any]) -> float:
     """
     function for sorting nodes by x
     :param node: dict with node data
@@ -248,21 +250,24 @@ def get_minmax_coord(nodes: [dict]) -> tuple:
     """
     min_x, min_y, max_x, max_y = 0, 0, 0, 0
     for node in nodes:
-        if 'y:GenericNode' in node.keys() or ('y:ProxyAutoBoundsNode' in node.keys() and 'y:Realizers' in node['y:ProxyAutoBoundsNode'].keys() and 'y:GroupNode' in node['y:ProxyAutoBoundsNode']['y:Realizers'].keys()):
-            temp_node = node['y:GenericNode'] if 'y:GenericNode' in node.keys() else node['y:ProxyAutoBoundsNode']['y:Realizers']['y:GroupNode']
+        if 'y:GenericNode' in node.keys() or \
+                ('y:ProxyAutoBoundsNode' in node.keys() and 'y:Realizers' in node['y:ProxyAutoBoundsNode'].keys()
+                 and 'y:GroupNode' in node['y:ProxyAutoBoundsNode']['y:Realizers'].keys()):
+            temp_node = node['y:GenericNode'] if 'y:GenericNode' in node.keys() \
+                else node['y:ProxyAutoBoundsNode']['y:Realizers']['y:GroupNode']
             temp_node = temp_node['y:Geometry']
-            x = int(float(temp_node['@x']))
-            y = int(float(temp_node['@y']))
-            w = int(float(temp_node['@width']))
-            h = int(float(temp_node['@height']))
-            min_x = min([min_x, x])
-            min_y = min([min_y, y])
-            max_x = max([max_x, x + w])
-            max_y = max([max_y, y + h])
+            x: int = int(float(temp_node['@x']))
+            y: int = int(float(temp_node['@y']))
+            w: int = int(float(temp_node['@width']))
+            h: int = int(float(temp_node['@height']))
+            min_x: int = min([min_x, x])
+            min_y: int = min([min_y, y])
+            max_x: int = max([max_x, x + w])
+            max_y: int = max([max_y, y + h])
     return min_x - 1, min_y - 1, max_x + 1, max_y + 1
 
 
-def get_state_actions(data: dict) -> str:
+def get_state_actions(data: Dict[str, Any]) -> str:
     """
     get label with actions from node data
     :param data: node with data
@@ -279,12 +284,12 @@ def get_state_actions(data: dict) -> str:
     return ""
 
 
-def get_group_actions(data: dict) -> str:
+def get_group_actions(data: Dict[str, Any]) -> str:
     """
-        get label with actions from group node data
-        :param data: node with data
-        :return: str with actions
-        """
+    get label with actions from group node data
+    :param data: node with data
+    :return: str with actions
+    """
     if 'y:ProxyAutoBoundsNode' not in data.keys():
         return ""
     data = data['y:ProxyAutoBoundsNode']
@@ -304,7 +309,7 @@ def get_group_actions(data: dict) -> str:
     return ""
 
 
-def get_state_label(data: dict) -> str:
+def get_state_label(data: Dict[str, Any]) -> str:
     """
     gets state label from node data
     :param data: dict with data
@@ -321,7 +326,7 @@ def get_state_label(data: dict) -> str:
     return ""
 
 
-def get_group_label(data: dict) -> str:
+def get_group_label(data: Dict[str, Any]) -> str:
     """
     gets group node label from node data
     :param data: dict with data
@@ -344,7 +349,7 @@ def get_group_label(data: dict) -> str:
     return ""
 
 
-def is_edge_correct(edge: dict, edge_type: str) -> bool:
+def is_edge_correct(edge: Dict[str, Any], edge_type: str) -> bool:
     """
     checks if edge contains all necessary data to be a correct edge with label
     :param edge: dict with edge data
@@ -358,7 +363,7 @@ def is_edge_correct(edge: dict, edge_type: str) -> bool:
     return True
 
 
-def get_edge_coordinates(edge: dict) -> tuple:
+def get_edge_coordinates(edge: Dict[str, Any]) -> Tuple[int, int, int, int, List[Tuple[int, int]]]:
     """
     function get edge coordinates (start as a shift from source state center, enf as a shift from source state center,
     points coordinates between them (as int)
@@ -366,18 +371,18 @@ def get_edge_coordinates(edge: dict) -> tuple:
     :return: (first_x, first_y, last_dx, last_dy, points)
     """
     coordinates = edge[edgetype]['y:Path']
-    x = int(float(coordinates['@sx']))
-    y = int(float(coordinates['@sy']))
-    last_dx = int(float(coordinates['@tx']))
-    last_dy = int(float(coordinates['@ty']))
-    points = []
+    x: int = int(float(coordinates['@sx']))
+    y: int = int(float(coordinates['@sy']))
+    last_dx: int = int(float(coordinates['@tx']))
+    last_dy: int = int(float(coordinates['@ty']))
+    points: List[Tuple[int, int]] = list()
     if 'y:Point' in coordinates.keys():
         coordinates = [coordinates['y:Point']] if isinstance(coordinates['y:Point'], dict) else coordinates['y:Point']
         points = [(int(float(point['@x'])), int(float(point['@y']))) for point in coordinates]
     return x, y, last_dx, last_dy, points
 
 
-def get_edge_label_coordinates(edge: dict) -> tuple:
+def get_edge_label_coordinates(edge: Dict[str, Any]) -> Tuple[int, int, int]:
     """
     gets coordinates for edge label
     :param edge: dict with edge data
@@ -385,6 +390,6 @@ def get_edge_label_coordinates(edge: dict) -> tuple:
     """
     if is_edge_correct(edge, edgetype):
         label = edge[edgetype]['y:EdgeLabel']
-        return int(float(label['@x'])), int(float(label['@y'])), int (float(label['@width']))
+        return int(float(label['@x'])), int(float(label['@y'])), int(float(label['@width']))
     else:
         return 0, 0, 0
