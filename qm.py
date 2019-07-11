55e62c490a3208cf4709aa634688b14abd2e6a39
Julia Salnikova (Notiel) <agereth@gmail.com>
2019-07-11 19:13:21 +0300

    dataclasses moved to separate file

--- qm.py
+++ qm.py
@@ -24,6 +24,7 @@ from dataclasses import *
 from graphml import *
 import re
 from typing import List, Optional
+from stateclasses import State, Trigger
 
 divider = 10  # we divide graphml coordinates by that value
 action_delta = 5  # addition to action box
@@ -33,74 +34,6 @@ global_h_delta = 10  # addition to global height
 global_w_delta = 10  # addition to global width
 terminal_w = 10
 terminal_h = 10
-
-"""
-   Class Trigger describes Triggers of uml-diagrams
-        name: name of trigger
-        type: internal or external
-        guard: text of trigger guard if any
-        source: source state of trigger (actual for external triggers)
-        target: target state of trigger (actual for external triggers)
-        action: action for this trigger if any
-        id: order number of internal trigger for better coordinates
-        x, y: start of trigger visual path
-        dx, dy: first relative movement of trigger visual path
-        points: other relative movements of trigger visual path
-        action_x, action_y, action_width: coordinates of trigger label
-"""
-
-
-@dataclass
-class Trigger:
-    name: str
-    source: str
-    target: str
-    action: str
-    id: int
-    x: int
-    y: int
-    dx: int
-    dy: int
-    points: List[Tuple[int, int]]
-    action_x: int
-    action_y: int
-    action_width: int
-    type: str = "internal"
-    guard: str = ""
-
-
-"""
-   class State describes state of uml-diagram and trigslates to qm format.
-   Fields:
-        name: name of state
-        type: state or choice
-        trigs: list of trigsitions from this state both external and internal
-        entry: action on entry event
-        exit: action on exit event
-        id: number of state
-        actions: raw_data for external actions
-        old_id: id of state in graphml
-        x, y: graphical coordinates
-        height, width: height and with of node
-"""
-
-
-@dataclass
-class State:
-    name: str
-    type: str
-    actions: str
-    trigs: List[Trigger]
-    entry: str
-    exit: str
-    id: str
-    new_id: List[str]
-    x: int
-    y: int
-    width: int
-    height: int
-    parent: Optional['State']
-    childs: List['State']
 
 
 def get_state_by_id(states: [State], state_id: Optional[str, State], id_type: str) -> State:
