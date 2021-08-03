f7caa9abbf9e7678a554352a4406d58737bf51a3
Alexey Eremin <a.eremin.msu@gmail.com>
2021-08-04 00:56:10 +0200

    Fix combining of triggers with separate name - support case when they are not consequent

--- graphml_to_cpp.py
+++ graphml_to_cpp.py
@@ -1,6 +1,7 @@
 import os.path
 
 import re
+from collections import defaultdict
 from graphml import *
 from typing import List, Tuple
 from stateclasses import State, Trigger
@@ -173,14 +174,19 @@ class CppFileWriter:
             self._insert_string('            break;\n')
             self._insert_string('        }\n')
 
-        triggers_merged: List[Tuple[str, List[Trigger]]] = []
-        for trigger in state.trigs:
+        name_to_triggers = defaultdict(list)
+        name_to_position = {}
+
+        for i, trigger in enumerate(state.trigs):
             if '?def' in trigger.name:
                 continue
-            if trigger.guard and len(triggers_merged) and triggers_merged[-1][0] == trigger.name:
-                triggers_merged[-1][1].append(trigger)
-            else:
-                triggers_merged.append((trigger.name, [trigger]))
+
+            name_to_triggers[trigger.name].append(trigger)
+            name_to_position[trigger.name] = i
+
+        triggers_merged: List[Tuple[str, List[Trigger]]] = sorted(
+            [(name, name_to_triggers[name]) for name in name_to_triggers],
+            key = lambda t: name_to_position[t[0]])
 
         for event_name, triggers in triggers_merged:
             self._insert_string('        /*.${%s::%s} */\n' % (state_path, event_name))
